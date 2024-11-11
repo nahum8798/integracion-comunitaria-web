20cad5a515768eec2417fd44fa5ea5aa7744d30e
nahuelmendez6 <mendezmaturano6@gmail.com>
2024-11-11 16:21:31 -0300

    Primer commit

---
+++ src/main/java/com/integracion_comunitaria/IntegracionComunitariaApplication.java
@@ -0,0 +1,13 @@
+package com.integracion_comunitaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegracionComunitariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegracionComunitariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/integracion_comunitaria/IntegracionComunitariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.integracion_comunitaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegracionComunitariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
